0e82d6c057e37fa99425ea0be32baf02c36011b5
Roy <royrao2333@gmail.com>
2020-07-14 17:53:17 +0800

    Add files via upload

---
+++ BinarySearchTree/BST.hpp
@@ -0,0 +1,180 @@
+//
+//  BST.hpp
+//  BinarySearchTree
+//
+//  Created by Roy Rao on 2020/7/14.
+//  Copyright © 2020 RoyRao. All rights reserved.
+//
+
+#ifndef BST_hpp
+#define BST_hpp
+
+#include <iostream>
+using namespace std;
+
+#endif /* BST_hpp */
+
+template<typename T> class BSTNode {
+public:
+    BSTNode(T _data): data(_data), leftChild(nullptr), rightChild(nullptr) {}
+public:
+    T data;
+    BSTNode *leftChild;
+    BSTNode *rightChild;
+};
+
+template<typename T> class BSTree {
+public:
+    BSTree();
+    ~BSTree();
+    BSTNode<T>* insert(T data);
+    BSTNode<T>* find(T data);
+    BSTNode<T>* findMinimum(BSTNode<T> *iter);
+    BSTNode<T>* findMaximum(BSTNode<T> *iter);
+    bool deleteWith(T data);
+    BSTNode<T>* getParent(BSTNode<T> *ptr);
+    int getLength();
+    void traverse(BSTNode<T> *ptr);
+    void clear();
+private:
+    BSTNode<T>* insert(BSTNode<T> *iter, T data);
+    BSTNode<T>* find(BSTNode<T> *iter, T data);
+    bool deleteWith(BSTNode<T> *iter, T data);
+    void clear(BSTNode<T> *iter);
+private:
+    BSTNode<T> *root;
+    int length;
+};
+
+
+
+
+// MARK: - CONSTRUCT AND DESTRUCT
+template<typename T> BSTree<T>::BSTree() {
+    this->root = nullptr;
+    this->length = 0;
+}
+
+template<typename T> BSTree<T>::~BSTree() {
+    this->clear();
+}
+
+// MARK: - INSERT
+template<typename T> BSTNode<T>* BSTree<T>::insert(T data) {
+    return insert(this->root, data);
+}
+
+template<typename T> BSTNode<T>* BSTree<T>::insert(BSTNode<T> *iter, T data) {
+    if (iter == nullptr) {
+        iter = new BSTNode<T>(data);
+    } else if (data < iter->data) {
+        iter->leftChild = insert(iter->leftChild, data);
+    } else if (data > iter->data) {
+        iter->rightChild = insert(iter->rightChild, data);
+    }
+    return iter;
+}
+
+// MARK: - DELETE
+template<typename T> bool BSTree<T>::deleteWith(T data) {
+    return this->deleteWith(this->root, data);
+}
+
+template<typename T> bool BSTree<T>::deleteWith(BSTNode<T> *iter, T data) {
+    if (iter == nullptr) {
+        cout << "No element to delete!" << endl;
+        return false;
+    }
+    BSTNode<T> *temp = nullptr;
+    if (data < iter->data) {
+        this->deleteWith(iter->leftChild, data);
+    } else if (data > iter->data) {
+        this->deleteWith(iter->rightChild, data);
+    } else {
+        if (iter->leftChild != nullptr && iter->rightChild != nullptr) {
+            /// two children
+            temp = this->findMinimum(iter->rightChild);
+            iter->data = temp->data;
+        } else {
+            /// have one or none
+            temp = iter;
+            if (iter->leftChild == nullptr) {
+                iter = iter->rightChild;
+            } else {
+                iter = iter->leftChild;
+            }
+        }
+        delete temp;
+        temp = nullptr;
+    }
+    return true;
+}
+
+// MARK: - FIND
+template<typename T> BSTNode<T>* BSTree<T>::find(T data) {
+    return this->find(this->root, data);
+}
+
+template<typename T> BSTNode<T>* BSTree<T>::find(BSTNode<T> *iter, T data) {
+    if (iter == nullptr) {
+        cout << "No element found!" << endl;
+        return nullptr;
+    }
+    if (data < iter->data) {
+        this->find(iter->leftChild, data);
+    } else if (data > iter->data) {
+        this->find(iter->rightChild, data);
+    } else {
+        return iter;
+    }
+}
+
+template<typename T> BSTNode<T>* BSTree<T>::findMinimum(BSTNode<T> *iter) {
+    if (iter == nullptr) {
+        cout << "No element found!" << endl;
+        return nullptr;
+    }
+    if (iter->leftChild == nullptr) {
+        return iter;
+    } else {
+        this->findMinimum(iter->leftChild);
+    }
+}
+
+template<typename T> BSTNode<T>* BSTree<T>::findMaximum(BSTNode<T> *iter) {
+    if (iter == nullptr) {
+        cout << "No element found!" << endl;
+        return nullptr;
+    }
+    if (iter->rightChild == nullptr) {
+        return iter;
+    } else {
+        this->findMaximum(iter->rightChild);
+    }
+}
+
+// MARK: - TOOLS
+template<typename T> void BSTree<T>::clear() {
+    if (this->root == nullptr) {
+        return;
+    }
+    this->clear(this->root);
+}
+
+template<typename T> void BSTree<T>::clear(BSTNode<T> *iter) {
+    if (iter->leftChild != nullptr) {
+        this->clear(iter->leftChild);
+        iter->leftChild = nullptr;
+    }
+    if (iter->rightChild != nullptr) {
+        this->clear(iter->rightChild);
+        iter->rightChild = nullptr;
+    }
+    delete iter;
+    iter = nullptr;
+    this->length--;
+}
+
+template<typename T> int BSTree<T>::getLength() {
+    return this->length;
+}
---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  BinarySearchTree
+//
+//  Created by Roy Rao on 2020/7/14.
+//  Copyright © 2020 RoyRao. All rights reserved.
+//
+
+#include <iostream>
+#include "BST.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    BSTree<int> tree;
+    
+    return 0;
+}
